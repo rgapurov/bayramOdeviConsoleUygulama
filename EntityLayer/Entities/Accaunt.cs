43c46ffea4bcbc1a39fa495201e19797b3fe2604
rgapurov <92965000+rgapurov@users.noreply.github.com>
2022-07-16 14:59:22 +0300

    Add project files.

---
+++ EntityLayer/Entities/Accaunt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Accaunt
+    {
+        [Key]
+        public int accountNo { get; set; }
+        [StringLength(20)]
+        public string acName { get; set; }
+        public int acBalance { get; set; }
+        public ICollection<Transaction> transactions { get; set; }
+
+
+    }
+}
---
+++ EntityLayer/Entities/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Transaction
+    {
+        [Key]
+        public int transactionNo { get; set; }
+        public int trAccaunt { get; set; }
+        public virtual Accaunt accountNo { get; set; }
+        public DateTime trTime { get; set; }
+        [StringLength(50)]
+        public string trType { get; set; }
+        public int trAmount { get; set; }
+    }
+}
